2e06dee9fdaf756b6f270adcf1c184ea55b8a15d
andNiAl <andrea_alva2@usmp.pe>
2025-09-28 00:33:26 -0500

    style: corrección de Sidebar

--- apps/dashboard/src/components/layout/Sidebar.tsx
+++ apps/dashboard/src/components/layout/Sidebar.tsx
@@ -4,12 +4,11 @@ import usmpLogo from "../../assets/Logo_FIA.png";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-[#111827] text-white h-screen flex flex-col">
-      {/* Logo */}
+    <aside className="w-64 bg-[#111827] text-white h-screen fixed top-0 left-0 flex flex-col">
+          {/* Logo */}
       <div className="flex items-center justify-center p-6 border-b border-gray-700">
         <img src={usmpLogo} alt="USMP Logo" className="h-16 object-contain" />
       </div>
-
       {/* Menu options */}
       <nav className="flex-1 p-4">
         <ul className="space-y-3">
